c4ba49ed95fb5018b1e78560ddf6e5bbb687a425
mehdi.dev.more <mehdi.dev.more@gmail.com>
2018-05-30 14:28:53 -0400

    test de la page home

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180527180738) do
+ActiveRecord::Schema.define(version: 20180527185612) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20180527180738) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string   "phone_number"
+    t.text     "desciption"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
